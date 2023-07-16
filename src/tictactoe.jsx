6666be84009812adbe27c78d01edb2d3e408e0d7
rakcurious <rakcurious@gmail.com>
2023-07-16 18:54:09 +0530

    some meta tags added and animation lib installed

--- src/tictactoe.jsx
+++ src/tictactoe.jsx
@@ -1,8 +1,8 @@
+import * as LottiePlayer from "@lottiefiles/lottie-player";
 import o from "./assets/icon_o.svg";
 import x from "./assets/icon_x.svg";
 import e from "./assets/icon_edit.svg";
 import { useState } from "react";
-import thumbnail from "./assets/ticthumbnail.png";
 
 function TicTacToe() {
   const [tic, setTic] = useState([e, e, e, e, e, e, e, e, e]);
@@ -69,7 +69,6 @@ function TicTacToe() {
       <h1 className="text-black text-4xl  text-center p-10 lg:p-3 font-bold font-mono ">
         Tic Tac Toe
       </h1>
-      <img className="h-0 w-0 hidden" src={thumbnail}/>
       <div className=" flex justify-center items-baseline">
         <h1
           className={
